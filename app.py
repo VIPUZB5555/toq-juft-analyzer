f615ef7a1a86184c1742bd96bae137bae5769199
VIPUZB5555 <ogabekor5555@gmail.com>
2025-05-31 10:43:59 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,25 +4,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    natija = None
+    juftlar = []
+    toqlar = []
+    son = None
     if request.method == 'POST':
         try:
-            son = int(request.form['son'])
-            juft_sonlar = [i for i in range(1, son + 1) if i % 2 == 0]
-            toq_sonlar = [i for i in range(1, son + 1) if i % 2 != 0]
-            info = {
-                'kiritilgan': son,
-                'toq_sonlar': toq_sonlar,
-                'juft_sonlar': juft_sonlar,
-                'toq_soni': len(toq_sonlar),
-                'juft_soni': len(juft_sonlar),
-                'eng_katta_juft': juft_sonlar[-1] if juft_sonlar else None,
-                'eng_katta_toq': toq_sonlar[-1] if toq_sonlar else None,
-            }
-            natija = info
+            son = int(request.form.get('son'))
+            for i in range(1, son+1):
+                if i % 2 == 0:
+                    juftlar.append(i)
+                else:
+                    toqlar.append(i)
         except:
-            natija = {'xato': 'Faqat butun son kiriting!'}
-    return render_template("index.html", natija=natija)
+            son = None
+    return render_template('index.html', son=son, juftlar=juftlar, toqlar=toqlar)
 
 if __name__ == '__main__':
     app.run(debug=True)
